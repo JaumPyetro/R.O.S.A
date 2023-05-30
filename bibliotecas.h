1b1a96756311e0b4fca7d67b2cf0530433da692d
João Pedro Araújo Nóbrega <130778610+JaumPyetro@users.noreply.github.com>
2023-05-30 04:24:01 -0300

    Create bibliotecas.h
    criação da página de bibliotecas
    criação das bibliotecas

---
+++ bibliotecas.h
@@ -0,0 +1,13 @@
+#ifndef BIBLIOTECAS_H
+#define BIBLIOTECAS_H
+
+void Regulamento();
+void login();
+void loginResidente();
+void loginReceptor();
+void loginGestor();
+void criarConta();
+void criarContaResidente();
+void criarContaReceptor();
+
+#endif  // BIBLIOTECAS_H
