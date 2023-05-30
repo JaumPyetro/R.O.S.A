58cbb5e8cefc0ee9aae866e164e57df7296996d8
João Pedro Araújo Nóbrega <130778610+JaumPyetro@users.noreply.github.com>
2023-05-30 04:19:35 -0300

    Update main.c
    ADICIONANDO O MAIN 1.01

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
+#include "bibliotecas.h"
+#include <stdio.h>
+
+int main() {
+  int opcao;
+  printf("BEM VINDO AO PROGRAMA ROSA\n");
+
+  do {
+    printf("\nEscolha uma opcao:\n");
+    printf("1. Login\n");
+    printf("2. Regulamento\n");
+    printf("0. Sair\n");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+    case 1:
+      login();
+      break;
+    case 2:
+      Regulamento();
+      opcao = 1; // Definir opcao como 1 para voltar ao menu principal
+      break;
+    case 0:
+      printf("Saindo do programa...\n");
+      break;
+    default:
+      printf("Opcao invalida!\n");
+      break;
+    }
+  } while (opcao != 0);
+
+  return 0;
+}
 
