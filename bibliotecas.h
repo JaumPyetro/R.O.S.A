3ce0a95da11466a5da4d8545a1faf709c15aed6e
João Pedro Araújo Nóbrega <130778610+JaumPyetro@users.noreply.github.com>
2023-06-13 03:57:40 -0300

    Update bibliotecas.h

--- bibliotecas.h
+++ bibliotecas.h
@@ -1,13 +1,39 @@
 #ifndef BIBLIOTECAS_H
 #define BIBLIOTECAS_H
 
-void Regulamento();
-void login();
-void loginResidente();
-void loginReceptor();
+void aplicativoRosa();
+void exibirRegulamento();
+void homeLogin();
+void homeGestor();
 void loginGestor();
 void criarConta();
 void criarContaResidente();
-void criarContaReceptor();
+void criarContaPreceptor();
+void homeTotem();
+int validate(char *fileName, char *inputAccessKey);
+void writeExitTime();
+void writeEntryTime();
+void loginPreceptor();
+void homePreceptor();
+void ListaDeResidentes();
+int loginResidente();
+void homeResidente();
+void calendario();
+void showTotalProgress();
+void dias();
+void progresso();
+void registro();
+void remove_duplicate_lines();
+void escolherResidente();
+void Avaliacao();
+int buscarResidente();
+int procurando();
+void realizar_acao();
+int procurar_nome_arquivo();
+void fichaAvaliativa();
+void imprimirFichaAvaliacao();
+void imprimirFeedback();
+void ListaDeResidentesGestor();
+void imprimirPreceptores();
 
-#endif  // BIBLIOTECAS_H
+#endif // BIBLIOTECAS_H
